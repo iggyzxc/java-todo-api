260e3122dfb1c6d026878b6039fcb6f05638c4ed
Iggy <ignaciomacam@gmail.com>
2025-02-09 11:04:08 +0800

    first commit

---
+++ src/main/java/org/iggyzxc/javatodoapi/JavaTodoApiApplication.java
@@ -0,0 +1,13 @@
+package org.iggyzxc.javatodoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTodoApiApplication.class, args);
+    }
+
+}
